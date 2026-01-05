c6f526cb46f00fabd62f90477d4ba9c292c52078
alfonsoz <fkeijzer@gmail.com>
2026-01-05 08:35:52 +0100

    add function to get book text

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+#open and return book text from specified path
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def main():
+    book = get_book_text('books/frankenstein.txt')
+    print(book)
+
+
+main()
