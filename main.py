d0d123c0571a91915671b8e95d21aeb4ed8d7418
alfonsoz <fkeijzer@gmail.com>
2026-01-05 09:19:31 +0100

    add letter count funtion

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from stats import word_count, get_book_text
+from stats import word_count, get_book_text, letter_count
 
 def main():
     book = get_book_text('books/frankenstein.txt')
     num_words = word_count(book)
     print(f"Found {num_words} total words")
+    lettercount = letter_count(book)
+    print(f"Letter count: {lettercount}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,16 @@ def get_book_text(path):
 #count words in text
 def word_count(text):
     return len(text.split())
+
+#count amount of duplicate letters in text
+def letter_count(text):
+    letter_dict = {}
+    #lowercase for every char
+    lowercase_text = text.lower()
+    for letter in lowercase_text:
+        if letter not in letter_dict:
+            letter_dict[letter] = 1
+        else:
+            letter_dict[letter] += 1
+    return letter_dict
+
